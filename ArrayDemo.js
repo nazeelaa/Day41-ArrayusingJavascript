06f249d27f17b96537579be36d63d3c24173b24c
nazeelaa <92171051+nazeelaa@users.noreply.github.com>
2021-12-20 00:07:18 +0530

    Array Practice Problems using Javascript

---
+++ ArrayDemo.js
@@ -0,0 +1,14 @@
+let oriDogs=["BullDOG","BEAGLE","LABRADOR"];
+let cars=new Array("American Curl","bENGAL");
+let brids=new Array("Falcons","Ducks","Flamingoes");
+let sliceDogs=oriDogs.Slice(0,2);
+console.log(sliceDogs);
+let copyDogs=[...originDogs];
+console.log(copyDogs);
+let dogs=oriDogs.slice(0);
+console.log(dogs);
+
+let pushDog=dogs.push("Golder Retriever");
+console.log(pushdogs);
+let popdog=dogs.pop();
+dogs[dogs.length]="poodle";
